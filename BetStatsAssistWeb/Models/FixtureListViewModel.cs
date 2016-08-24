522146181edace73b0f327b25a10e75463d3c77d
Charizanis Stelios <stehar7@hotmail.com>
2016-08-24 18:40:16 +0300

    add models

---
+++ BetStatsAssistWeb/Models/FixtureListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetStatsAssistWeb.Models
+{
+    public class FixtureListViewModel
+    {
+        public List<FixtureViewModels> Fixtures { get; set; }
+    }
+}
---
+++ BetStatsAssistWeb/Models/FixtureViewModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetStatsAssistWeb.Models
+{
+    public class FixtureViewModels
+    {
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+        public string FixDate { get; set; }
+    }
+}
